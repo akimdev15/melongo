3fab7aa601120b47f01ba2e08c6f61cef25de193
Aisen Kim <aisenkim15@gmail.com>
2024-02-27 23:11:49 -0500

    Refactored daily music scrape to use go routines.

--- handler_music.go
+++ handler_music.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/akimdev15/melongo/internal/database"
@@ -16,23 +17,37 @@ func (apiCfg *apiConfig) handleDailyMusicScrape() {
 	genres, err := apiCfg.DB.GetAllGenre(context.Background())
 	if err != nil {
 		fmt.Printf("Error getting the genre code from the DB. Err: %s", err)
+		return
 	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(genres))
+
 	for _, genre := range genres {
-		songs := mscraper.GetNewestSongsMelon(genre.Code)
-		for _, song := range songs {
-			_, err := apiCfg.DB.CreateMusic(context.Background(), database.CreateMusicParams{
-				ID:        uuid.New(),
-				Name:      song.Title,
-				Artist:    song.Artist,
-				CreatedAt: time.Now().UTC(),
-				Genre:     genre.ID,
-			})
-			if err != nil {
-				fmt.Printf("Error creating the music: %v.\n  Error: %s", song, err)
-			}
-		}
-		fmt.Printf("Saved %d songs for genre: %s", len(songs), genre.Name)
+		go func(genre database.Genre) {
+			defer wg.Done()
+			scrapeAndSaveSongs(apiCfg, genre)
+		}(genre)
 	}
+
+	wg.Wait()
+}
+
+func scrapeAndSaveSongs(apiCfg *apiConfig, genre database.Genre) {
+	songs := mscraper.GetNewestSongsMelon(genre.Code)
+	for _, song := range songs {
+		_, err := apiCfg.DB.CreateMusic(context.Background(), database.CreateMusicParams{
+			ID:        uuid.New(),
+			Name:      song.Title,
+			Artist:    song.Artist,
+			CreatedAt: time.Now().UTC(),
+			Genre:     genre.ID,
+		})
+		if err != nil {
+			fmt.Printf("Error creating the music: %v. Error: %s\n", song, err)
+		}
+	}
+	fmt.Printf("Saved %d songs for genre: %s\n", len(songs), genre.Name)
 }
 
 // handleGetMusic - using query parameters. Either by title, artist or both
